709556639d4f79cdc5e2fd22dc2e1aceb9f1064e
Gabriela Lemos <gabriela.leemoss@gmail.com>
2019-10-31 20:02:05 -0300

    Fila circular com redimensionamento

--- fila_circular.h
+++ fila_circular.h
@@ -1,3 +1,30 @@
+// circular com redimensionamento
+
+typedef struct fila_circular{
+	int* data;
+	int tamanho;
+	int capacidade;
+
+	int p;
+	int u;
+}fila_circular;
+
+void cria_fila();
+
+int insere_elemento(int x);
+
+int remove_elemento(int *valor);
+
+int fila_cheia(fila_circular* fila);
+
+int fila_vazia();
+
+int tamanho_fila();
+
+void imprime_fila();
+
+
+/* fila lista circular
 #define TAMANHO_FILA	5
 
 typedef struct celula{
@@ -18,3 +45,4 @@ int fila_vazia();
 int tamanho_fila();
 
 void imprime_fila();
+*/
